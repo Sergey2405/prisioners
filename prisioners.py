d8f89b5d72405f2231e0fd629f21645fd2079c2c
Sergey2405 <grachevse2405@google.com>
2022-09-11 22:50:19 +0300

    +trim_statistics

--- prisioners.py
+++ prisioners.py
@@ -57,15 +57,36 @@ class Prisioners(Boxes):
       results[prisioner] = self.open_sequence_of_boxes(prisioner, offset)
     return results
 
-  def statisctics(self):
-    statisctics = {}
+  def statistics(self):
+    statistics = {}
     for offset in range(0, self.number):
-      statisctics[offset] = self.open_sequence_of_boxes_by_all_the_prisioners(offset)
-    return statisctics
+      statistics[offset] = self.open_sequence_of_boxes_by_all_the_prisioners(offset)
+    return statistics
+
+  def trim_statistics(self):
+    statistics = self.statistics()
+    print("statistics=",statistics)
+    for token_offset in statistics:
+      # print("token_stat", token_stat)# int
+      # print("statistics[token_stat]=", statistics[token_stat]) # statistics[token_stat]= {0: ([(6, 0)], [(5, 6), (1, 1), (6, 0)]), 1: ([(1, 1)], [(6, 0), (5, 6), (1, 1)]), 2: ([(7, 2)], [(7, 2)]), 3: ([(0, 3)], [(8, 7), (2, 8), (3, 5), (0, 3)]), 4: ([(4, 4)], [(9, 9), (4, 4)]), 5: ([(3, 5)], [(0, 3), (8, 7), (2, 8), (3, 5)]), 6: ([(5, 6)], [(1, 1), (6, 0), (5, 6)]), 7: ([(8, 7)], [(2, 8), (3, 5), (0, 3), (8, 7)]), 8: ([(2, 8)], [(3, 5), (0, 3), (8, 7), (2, 8)]), 9: ([(9, 9)], [(4, 4), (9, 9)])}
+      token_stat = statistics[token_offset]
+      token_stat['unreleased'] = 0
+      token_stat['released'] = self.number
+      print("token_stat=", token_stat)
+      for token_prisioner in token_stat:
+        opened_boxes = token_stat[token_prisioner]
+        print("opened_boxes=",opened_boxes)
+        if token_prisioner == 'unreleased' or token_prisioner == 'released':
+          continue
+        if opened_boxes[0] == 'depth':
+          token_stat['unreleased'] += 1
+          token_stat['released'] -= 1
+      statistics[token_offset] = token_stat
+    return statistics
 
   def shuffle_up_boxes(self, sequence):
-  	for token in sequence:
-  	  self.boxes.swap(token[0], token[1])
+    for token in sequence:
+      self.boxes.swap(token[0], token[1])
 
 
 prisioners10 = Prisioners(10)
@@ -77,3 +98,4 @@ print("After shuffling up boxex. prisioners10.get_boxes",prisioners10.boxes.boxe
 print("prisioners10.boxes.get_box(5)=",prisioners10.boxes.get_box(5))
 print("prisioners10.get_box(5)=",prisioners10.get_box(5))
 print("prisioners10.open_sequence_of_boxes(5, 8)",prisioners10.open_sequence_of_boxes(5, 8))
+print("prisioners10.trim_statistics()=", prisioners10.trim_statistics())
